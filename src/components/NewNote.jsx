b4c3f7fa365adf00ee6a9012346ec03058077b76
rayaadinda <rayaadinda78@gmail.com>
2024-07-23 14:09:40 +0700

    add new color every create new note

--- src/components/NewNote.jsx
+++ src/components/NewNote.jsx
@@ -4,6 +4,13 @@ const NewNote = ({ handleAddNote }) => {
 	const [noteText, setNoteText] = useState("")
 	const characterLimit = 200
 
+	const colors = ["#c084fc", "#fb7185", "#a3e635", "#38bdf8"]
+	const getRandomColor = () => {
+		return colors[Math.floor(Math.random() * colors.length)]
+	}
+
+	const [backgroundColor, setBackgroundColor] = useState(getRandomColor())
+
 	const handleChange = (event) => {
 		if (characterLimit - event.target.value.length >= 0) {
 			setNoteText(event.target.value)
@@ -14,18 +21,23 @@ const NewNote = ({ handleAddNote }) => {
 		if (noteText.trim().length > 0) {
 			handleAddNote(noteText)
 			setNoteText("")
+			setBackgroundColor(getRandomColor())
 		}
 	}
 
 	return (
-		<div className="bg-cyan-400 rounded-lg p-4 mb-3   flex flex-col items-center">
+		<div
+			className="rounded-lg p-4 mb-3 flex flex-col items-center"
+			style={{ backgroundColor }}
+		>
 			<textarea
-				className="bg-cyan-400 border-none resize-none w-full h-40"
+				className="border-none resize-none w-full h-40"
 				rows={4}
 				cols={10}
 				placeholder="Enter your note"
 				value={noteText}
 				onChange={handleChange}
+				style={{ backgroundColor }}
 			></textarea>
 			<div className="flex items-center justify-between w-full">
 				<small>{characterLimit - noteText.length} Remaining</small>
